28b313d0e9acd1e9f93f21e69c94eb566279d304
Evhenii <amaxasla322@gmail.com>
2019-07-08 19:41:43 +0300

    'hookah'

--- js/clicker.js
+++ js/clicker.js
@@ -9,13 +9,16 @@ let cursorCount=document.querySelector('.cursor__counter');
 let schoolCount=document.querySelector('.school__counter');
 let lmCount=document.querySelector('.LM__counter');
 let stickCount=document.querySelector('.stick__counter');
+let hookahCount=document.querySelector('.hookah__counter');
 let schoolPrice=document.querySelector('.school__price');
 let cursorPrice=document.querySelector('.cursor__price');
 let lmPrice=document.querySelector('.LM__price');
 let stickPrice=document.querySelector('.stick__price');
+let hookahPrice=document.querySelector('.hookah__price');
 let lm=document.querySelector('.LM');
 let whistle=document.querySelector('.whistle');
 let stick=document.querySelector('.stick');
+let hookah=document.querySelector('.hookah');
 let whistlePrice=document.querySelector('.whistle__price');
 let whistleCount=document.querySelector('.whistle__counter');
 let counterNumber=0;
@@ -25,28 +28,33 @@ let cursorBought=false;
 let lmBought=false;
 let whistleBought=false;
 let stickBought=false;
+let hookahBought=false;
 let schoolCost=450;
 let cursorCost=50;
 let lmCost=100;
+let hookahCost=170;
 let whistleCost=200;
 let stickCost=330;
-let clickMultiplier=500;
+let clickMultiplier=30;
 let cursorPerSecond=0.2;
-let lmPerSecond=0.7;
-let schoolPerSecond=2;
-let whistlePerSecond=1.2;
-let stickPerSecond=1.7;
+let lmPerSecond=0.5;
+let hookahPerSecond=1.1;
+let whistlePerSecond=1.7;
+let stickPerSecond=3;
+let schoolPerSecond=4;
 let bitsPerSecond=0;
 let schoolCounter=0;
 let cursorCounter=0;
 let lmCounter=0;
 let whistleCounter=0;
 let stickCounter=0;
+let hookahCounter=0;
 let cursor=document.querySelector('.cursor');
 school.addEventListener('click',buySchool);
 cursor.addEventListener('click',buyCursor);
 lm.addEventListener('click',buyLm);
 whistle.addEventListener('click',buyWhistle);
+hookah.addEventListener('click',buyHookah);
 stick.addEventListener('click',buyStick);
 vanya.addEventListener('mousedown',click);
 vanya.addEventListener('mouseup',clickEffects);
@@ -55,6 +63,7 @@ cursorPrice.innerHTML='Cursor price:'+cursorCost;
 lmPrice.innerHTML='LM price:'+lmCost;
 whistlePrice.innerHTML='Whistle price:'+whistleCost;
 stickPrice.innerHTML='Stick price:'+stickCost;
+hookahPrice.innerHTML='Hookah price:'+hookahCost;
 
 
 function click() {
@@ -72,6 +81,7 @@ setInterval(function () {
         lmPrice.innerHTML='LM price:'+Math.round(lmCost);
         whistlePrice.innerHTML='Whistle price:'+Math.round(whistleCost);
         stickPrice.innerHTML='Stick price:'+Math.round(stickCost);
+        hookahPrice.innerHTML='Hookah price:'+Math.round(hookahCost);
         addBitsPerSecond();
         console.log(whistlePerSecond + 'свисток');
         console.log(schoolPerSecond+'школа');
@@ -95,6 +105,20 @@ function buyLm() {
         lmCost+=lmCost/3;
     }
 }
+
+function buyHookah() {
+    if (counterNumber>=hookahCost){
+        counter.innerHTML=counterNumber-hookahCost+' bits';
+        counterNumber-=hookahCost;
+        hookahCounter+=1;
+        bitsPerSecond+=hookahPerSecond;
+        hookahPerSecond+=hookahPerSecond/6;
+        hookahBought=true;
+        hookahCount.innerHTML=hookahCounter;
+        hookahCost+=hookahCost/3;
+    }
+}
+
 
 function buySchool() {
     if (counterNumber>=schoolCost){
@@ -161,11 +185,14 @@ let lmPanel=document.querySelector('.LM__panel');
 let whistlePanel=document.querySelector('.whistle__panel');
 let schoolPanel=document.querySelector('.school__panel');
 let stickPanel=document.querySelector('.stick__panel');
+let hookahPanel=document.querySelector('.hookah__panel');
 setInterval(enoughMoneyForCursor,100);
 setInterval(enoughMoneyForLm,100);
 setInterval(enoughMoneyForWhistle,100);
 setInterval(enoughMoneyForSchool,100);
 setInterval(enoughMoneyForStick,100);
+setInterval(enoughMoneyForHookah,100);
+
 function enoughMoneyForCursor() {
     if (counterNumber >= cursorCost) {
         cursorPanel.style.opacity = "1";
@@ -216,6 +243,16 @@ function enoughMoneyForStick() {
     } else if (counterNumber < stickCost) {
         stickPanel.style.opacity = "0.5";
        stickPanel.style.color = "red";
+    }
+}
+
+function enoughMoneyForHookah() {
+    if (counterNumber >= hookahCost) {
+        hookahPanel.style.opacity = "1";
+        hookahPanel.style.color = "green";
+    } else if (counterNumber < hookahCost) {
+        hookahPanel.style.opacity = "0.5";
+        hookahPanel.style.color = "red";
     }
 }
 setInterval(function kek () {
